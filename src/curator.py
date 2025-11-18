695d2521763ae12c7e84572de29b26dfc0b9b7b6
jung0729-debug <jung0729@g.skku.edu>
2025-11-18 13:25:42 +0900

    Update curator.py

--- src/curator.py
+++ src/curator.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 from openai import OpenAI
 
 # 네 API 키를 여기 넣으면 됩니다
-OPENAI_KEY = "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"
+OPENAI_KEY = "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"
 client = OpenAI(api_key=OPENAI_KEY) if OPENAI_KEY else None
 
 SYSTEM_PROMPT = (
